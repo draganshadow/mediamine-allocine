eecb60380e6d8805d42a383c049eccfe6e254a6b
Gael Cosnard <gc@netsyos.com>
2014-03-15 23:02:35 +0100

    Small test

--- config/services.config.php
+++ config/services.config.php
@@ -1,4 +1,17 @@
 <?php
 namespace MediaMine\Module\Allocine;
+use MediaMine\Module\Allocine\Tunnel\Allocine\AllocineTunnel;
+
 return array(
+    'factories' => array(
+        'AllocineTunnel' => function ($sm) {
+                $tunnel = new AllocineTunnel();
+                $tunnel->setLogger($sm->get('mediamine-tunnel-log'));
+                return $tunnel;
+            },
+        'AlloHelper' => function ($sm) {
+                $helper = new \AlloHelper();
+                return $helper;
+            },
+    ),
 );
--- src/MediaMine/Module/Allocine/Tunnel/Allocine/AllocineTunnel.php
+++ src/MediaMine/Module/Allocine/Tunnel/Allocine/AllocineTunnel.php
@@ -1,19 +1,62 @@
 <?php
 namespace MediaMine\Module\Allocine\Tunnel\Allocine;
 
-use MediaMine\Tunnel\AbstractTunnel;
-use MediaMine\Tunnel\Allocine\Parser\PersonParser;
+use MediaMine\Core\Entity\Tunnel\Person;
+use MediaMine\Core\Tunnel\AbstractTunnel;
+use MediaMine\Core\Tunnel\PersonImport;
 
-class AllocineTunnel extends AbstractTunnel
+class AllocineTunnel extends AbstractTunnel implements PersonImport
 {
+    const ALLOCINE = 'allocine';
+
+    /**
+     * Return tunnel name
+     * @return string
+     */
+    function getTunnelName()
+    {
+        return self::ALLOCINE;
+    }
+
+    /**
+     * Return array of handled entities and fields
+     * @return array
+     */
     public function getAbilities() {
         return array(
             'Person' => array()
         );
     }
 
-    public function searchPerson($name) {
-        $personParser = new PersonParser();
-        return $personParser->parse($name);
+    /**
+     * {@inheritdoc}
+     */
+    public function importPerson($id) {
+        $personRepo = $this->getRepository('Common\Person');
+        $sourcePerson = $personRepo->find($id);
+        $alloHelper = new \AlloHelper();
+        $result = $alloHelper->search($sourcePerson->name, 1, 10, false, array('person'));
+        if ($result->count()) {
+            $persons = $result->person->getArray();
+            if (count($persons) > 0) {
+                $result = $alloHelper->person($persons[0]['code'], 'medium')->getArray();
+                $person = new Person();
+                $person->exchangeArray(array(
+                    'tunnel' => self::ALLOCINE,
+                    'rid' => 0,
+                    'name' => $result['name']['given'] . ' ' . $result['name']['family'],
+                    'firstName' => $result['name']['given'],
+                    'lastName' => $result['name']['family'],
+                    'country' => $result['nationality']['$'],
+                    'birthDate' => new \DateTime($result['birthDate']),
+                    'deathDate' => $result['deathDate'] ? new \DateTime($result['deathDate']) : null,
+                    'summary' => $result['biography'],
+                    'images' => null,
+                    'raw' => json_encode($result),
+                ));
+                return $person;
+            }
+        }
+        return false;
     }
 }
