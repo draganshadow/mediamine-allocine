9adb33704954c2db0c69a8ee227da5780698b67f
draganshadow <gc@netsyos.com>
2014-06-01 23:38:46 +0200

    Fix

--- config/module.config.php
+++ config/module.config.php
@@ -11,18 +11,18 @@ return array(
             __DIR__ . '/../view',
         ),
     ),
-    'doctrine' => array(
-        'driver' => array(
-            __NAMESPACE__ .'_driver' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'cache' => 'array',
-                'paths' => array(__DIR__ . '/../src/' . rtrim(strtr(__NAMESPACE__, '\\', '/')) . '/Entity')
-            ),
-            'orm_default' => array(
-                'drivers' => array(
-                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
-                )
-            )
-        ),
-    ),
+//    'doctrine' => array(
+//        'driver' => array(
+//            __NAMESPACE__ .'_driver' => array(
+//                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+//                'cache' => 'array',
+//                'paths' => array(__DIR__ . '/../src/' . rtrim(strtr(__NAMESPACE__, '\\', '/')) . '/Entity')
+//            ),
+//            'orm_default' => array(
+//                'drivers' => array(
+//                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+//                )
+//            )
+//        ),
+//    ),
 );
